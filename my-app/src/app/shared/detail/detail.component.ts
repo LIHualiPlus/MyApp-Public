3ec1520770bb5439fa0c298bdec018b14889401e
lihl <lihl@1handcar.com>
2018-08-02 19:25:41 +0800

    11

--- my-app/src/app/shared/detail/detail.component.ts
+++ my-app/src/app/shared/detail/detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-detail',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
+  @Input() data: any;
   constructor() { }
 
   ngOnInit() {
